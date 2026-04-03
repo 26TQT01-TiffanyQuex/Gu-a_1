6f016ee6d1e3624ac01239925974af79db400c94
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-03 10:53:08 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int edad;
+        Console.WriteLine("Ingrese su edad actual");
+        edad = Convert.ToInt32(Console.ReadLine());
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Eres menor de edad");
+        }
+    }
+}
