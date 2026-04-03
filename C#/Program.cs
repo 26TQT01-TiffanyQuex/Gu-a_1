6df2436b82201b8273504bf8dc6fe4477a3c6214
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-03 15:55:27 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double grado1, grado2, grado3;
+   
+        Console.WriteLine("Ingrese uno de los grados del triángulo");
+        grado1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese otro de los grados del triángulo");
+        grado2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el tercer grado del triángulo");
+        grado3 = double.Parse(Console.ReadLine());
+        if (grado1 + grado2 + grado3 == 180)
+        {
+            if (grado1 == grado2 && grado2 == grado3)
+                Console.WriteLine("Es un triángulo equilátero");
+           
+            else if (grado1 == grado2 || grado2 == grado3 || grado3 == grado1)
+                Console.WriteLine("Es un triángulo isósceles");
+
+            else
+                Console.WriteLine("Es un triángulo escaleno");
+        }
+    }
+}
