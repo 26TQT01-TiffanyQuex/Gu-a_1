a53158e033d21567468267f860775e4c2ae4803f
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-04 17:59:10 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string PASSWORDT,PASSWORD;
+        PASSWORDT = "Password123";
+        Console.WriteLine("Ingrese la contraseña");
+        PASSWORD = Console.ReadLine();
+        if (PASSWORD == PASSWORDT)
+            { Console.WriteLine("Bienvenido"); }
+        else
+            { Console.WriteLine("Contraseña incorrecta"); }
+    }
+}
