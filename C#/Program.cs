0d856f5cc8640081e333c121b7c4404b1dcb1f3f
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-04 18:09:02 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double nota;
+        Console.WriteLine("Escriba la nota final");
+        nota = double.Parse(Console.ReadLine());
+        if (nota >= 70)
+            Console.WriteLine("Alumno aprobado");
+        else
+            Console.WriteLine("Alumno reprobado");
+    }
+}
