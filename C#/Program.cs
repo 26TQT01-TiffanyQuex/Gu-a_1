ad7d4371857354e98ec0ec459a7898721b7a31bc
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-04 18:03:52 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Tracing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dia;
+        Console.WriteLine("Escriba en número el día de la semana");
+        dia = Convert.ToInt32(Console.ReadLine());
+        if (dia >= 1 && dia <= 5)
+            Console.WriteLine("Es entre semana");
+        else
+            Console.WriteLine("Es fin de semana");
+    }
+}
