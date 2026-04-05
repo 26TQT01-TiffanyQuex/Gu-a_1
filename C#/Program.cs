d75bcb8dfd461cea3f29a7cb95e734392d4056d4
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-04 18:10:56 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dia;
+        Console.WriteLine("Escriba el número de día de la semana");
+        dia = Convert.ToInt32(Console.ReadLine());
+        if (dia == 1)
+            Console.WriteLine("Hoy es lunes");
+        if (dia == 2)
+            Console.WriteLine("Hoy es martes");
+        if (dia == 3)
+            Console.WriteLine("Hoy es miércoles");
+        if (dia == 4)
+            Console.WriteLine("Hoy es jueves");
+        if (dia == 5)
+            Console.WriteLine("Hoy es viernes");
+        if (dia == 6)
+            Console.WriteLine("Hoy es sábado");
+        if (dia == 7)
+            Console.WriteLine("Hoy es domingo");
+    }
+}
